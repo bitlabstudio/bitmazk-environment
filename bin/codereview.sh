9cb650566df1e25e6561974be62393c463b6f505
Martin Brochhhaus <mbrochh@googlemail.com>
2011-01-05 16:41:24 +0100

    working with absolute paths now instead of cd

--- bin/codereview.sh
+++ bin/codereview.sh
@@ -7,10 +7,10 @@
 # By running something like 'codereview.sh 2hd4H7..a83ho7' you can upload your
 # changeset to our internal codereview tool.
 
-cd $HOME/bin
-if [ ! -f upload.py ]
+if [ ! -f $HOME/bin/upload.py ]
 then
-wget http://codereview.appspot.com/static/upload.py
-chmod +x upload.py
+wget -O $HOME/bin/upload.py  http://codereview.appspot.com/static/upload.py
+chmod +x $HOME/bin/upload.py
 fi
 upload.py -s codereview.bitmazk.com $*
+
