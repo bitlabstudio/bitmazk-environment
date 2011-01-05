67bb505e8681837850d905c9b5f233c97b734618
Martin Brochhhaus <mbrochh@googlemail.com>
2011-01-05 13:49:59 +0100

    Added some comments to codereview.sh

--- bin/codereview.sh
+++ bin/codereview.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+# This file will check for Google's upload.py and download it if it does not
+# exist. See http://code.google.com/p/rietveld/wiki/CodeReviewHelp for more
+# information.
+
+# By running something like 'codereview.sh 2hd4H7..a83ho7' you can upload your
+# changeset to our internal codereview tool.
+
 cd $HOME/bin
 if [ ! -f upload.py ]
 then
